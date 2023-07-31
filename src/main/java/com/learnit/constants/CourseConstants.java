0c46c9e02bd2bdd79c2666ec7eaab1345a4bd782
partha <parthamanna.bit23@gmail.com>
2023-07-31 14:15:01 +0530

    course constant update

--- src/main/java/com/learnit/constants/CourseConstants.java
+++ src/main/java/com/learnit/constants/CourseConstants.java
@@ -5,6 +5,5 @@ public class CourseConstants {
 	public static final String PAGE_SIZE="10";
 	public static final String SORT_BY="rating";
 	public static final String SORT_DIR="ascending";
-	
-	public static final String thumbnailBaseUrl="http://localhost:8090/api/courses/thumbnail/";
+
 }
